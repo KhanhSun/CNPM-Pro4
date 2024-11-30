40bd8be71745d5dece65a93bae5a45a493b02870
BaoSonPie <167592310+BaoSonPie@users.noreply.github.com>
2024-11-30 15:35:26 +0700

    sách nè PI CÀ PO

---
+++ CNPM-Pro4/Controllers/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CNPM_Pro4.Models;
+namespace CNPM_Pro4.Controllers
+{
+    public class SearchController : Controller
+    {
+        BookEntities data = new BookEntities();
+        // GET: Search
+        [HttpGet]
+        public ActionResult Search(string strSearch = "")
+        {
+            ViewBag.strSearch = strSearch;
+            if (string.IsNullOrEmpty(strSearch))
+            {
+                return View();
+            }
+            else
+            {
+                var kq = data.saches.AsQueryable();
+
+                // Kiểm tra và lọc theo maSach nếu strSearch là số nguyên
+                if (int.TryParse(strSearch, out int maSach))
+                {
+                    kq = kq.Where(s => s.maSach == maSach.ToString());
+                }
+
+                // Lọc theo tenSach và các thuộc tính khác
+                kq = kq.Where(s => s.tenSach.Contains(strSearch) ||
+                                   s.maSach.Contains(strSearch) ||
+                                   s.isbn.Contains(strSearch));
+
+                // Sắp xếp theo tên sách (tenSach) và mã sách (maSach)
+                kq = kq.OrderBy(s => s.tenSach)
+                       .ThenBy(s => s.maSach);
+
+                var result = kq.ToList();
+                return View(result);
+            }
+        }
+
+
+    }
+}
